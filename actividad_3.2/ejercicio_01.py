ccf5f3a908386e701d40c639366b1ee060c71b87
o-aguirre <two.aguirre@gmail.com>
2024-06-03 17:00:01 -0400

    2024-06-03

--- actividad_3.2/ejercicio_01.py
+++ actividad_3.2/ejercicio_01.py
@@ -2,11 +2,14 @@ import random
 
 matriz_3D = []
 
-for x in range(4):
-    lista = []
+for x in range(3):
+    lista_ = []
+    matriz_3D.append(lista_)
     for y in range(4):
-        elemento = random.randint(1, 10)
-        matriz_3D.append(elemento)
-    
+        lista = []
+        lista_.append(lista)
+        for e in range(4):
+            elemento = random.randint(1, 3)
+            lista.append(elemento)
 
 print(matriz_3D)
