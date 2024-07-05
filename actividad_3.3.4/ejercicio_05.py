1ba6e04feb2c6699a322d9b26a48d8e8c84e5bb1
o-aguirre <two.aguirre@gmail.com>
2024-07-05 17:26:11 -0400

    Ejercicios funciones terminados

--- actividad_3.3.4/ejercicio_05.py
+++ actividad_3.3.4/ejercicio_05.py
@@ -24,4 +24,47 @@
 #- WOM
 #- Movistar
 #- Claro
+import json
 
+def to_dictionary(name, id, address, water, gas, electricity, internet, phone):
+    user_info = {
+        'nombre': name,
+        'rut': id,
+        'direccion': address,
+        'servicios':{
+            'agua': water,
+            'gas': gas,
+            'electricidad': electricity,
+            'internet': internet,
+            'telefono': phone
+        }
+    }
+    return user_info
+
+def to_json(data):
+    with open('user.json', 'w') as file:
+        data_json = json.dump(data, file)
+    return data_json
+
+user = []
+#
+while True:
+    print('\n1. Ingresa un usuario: ')
+    print('0. Salir: ')
+    opt = input(': ')
+
+    if opt == '1':
+        name = input('\nIngrese nombre: ')
+        id = input('Ingrese rut: ')
+        address = input('Ingrese direccion: ')
+        #
+        water = input('\nEscribe el nombre de tu compañia:\nEsval\nAguas Andinas\n: ')
+        gas = input('\nEscribe el nombre de tu compañia:\nLipigas\nGasco\nAbastible\n: ')
+        electricity = input('\nEscribe el nombre de tu compañia:\nCGE\nColbun\nChilquinta\nChilectra\n: ')
+        internet = input('\nEscribe el nombre de tu compañia:\nVTR\nMovistar\nEntel\n: ')
+        phone = input('\nEscribe el nombre de tu compañia:\nEntel\nWOM\nMovistar\nClaro\n: ')
+        #
+        user.append(to_dictionary(name, id, address, water, gas, electricity, internet, phone))
+    else:
+        to_json(user)
+        break
