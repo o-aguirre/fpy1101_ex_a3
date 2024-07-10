201f08bbc1c2a0f6a52c0cd2b9611ee55f79d46c
o-aguirre <two.aguirre@gmail.com>
2024-07-10 16:39:40 -0400

    Ejercicio 3.3.6 terminado

--- actividad_3.3.6/script.py
+++ actividad_3.3.6/script.py
@@ -1,4 +1,5 @@
 import csv
+import json
 
 def leer_datos(archivo):
     with open(archivo, 'r', encoding='utf-8') as file:
@@ -10,20 +11,20 @@ def leer_datos(archivo):
 interacciones = leer_datos('interacciones.csv')
 usuarios = leer_datos('usuarios.csv')
 
-#Terminar
 def contar_likes(data, user):
     contador_likes = 0
     for i in data:
-        if i[1] == user:
-            if
-            contador_likes += 1
+        if i[0] == user:
+            if i[2] == 'like':
+                contador_likes += 1
     return contador_likes
 
-def contar_comentarios(data):
+def contar_comentarios(data, user):
     contador_comentarios = 0
     for i in data:
-        if i[2] == 'comment':
-            contador_comentarios += 1
+        if i[0] == user:
+            if i[2] == 'comment':
+                contador_comentarios += 1
     return contador_comentarios
 
 def total_interacciones(likes, comments):
@@ -37,43 +38,48 @@ def calcular_edad(data, user):
             edad = int(edad[:4])
     return 2024 - edad  
 
-def ordenar(lista):
-    return lista[1]
-
-def mas_viciosos(interacciones, usuarios):
+def top_usuarios(interacciones):
+    usuarios = []
     interacciones_usuarios = []
+    for i in interacciones:
+        if i[0] not in usuarios: usuarios.append(i[0])
     for i in usuarios:
         contador_interacciones = 0
         for j in interacciones:
-            if i[0] == j[0]:
-                contador_interacciones += 1
-        interacciones_usuarios.append([i[1], contador_interacciones])   
-    interacciones_usuarios.sort(key=ordenar)
+            if i == j[0]: contador_interacciones += 1
+        interacciones_usuarios.append([i, contador_interacciones])
+    interacciones_usuarios = sorted(interacciones_usuarios, key=lambda e: e[1], reverse=True)
+    top_tres_usuarios = interacciones_usuarios[0:3]
+    return top_tres_usuarios
 
-    usuarios_mas_activos = interacciones_usuarios[-3:]
-    print(usuarios_mas_activos)
+def es_top_usuario(interacciones, user):
+    for i in interacciones:
+        if user in i[0]: return True
+    return False
 
-def to_diccionario(usuarios):
+
+def escribir_reporte(usuarios):
     dic_usuarios = []
     for i in usuarios:
         user = {
-            'id' : i[1],
+            'nombre' : i[1],
+            'id' : i[0],
             'edad' : calcular_edad(usuarios, i[1]),
-            #terminar
-            'total_likes' : contar_likes(interacciones),
-            'total_comments' : contar_comentarios(interacciones),
-            'numero_interacciones' : total_interacciones(contar_likes(interacciones), contar_comentarios(interacciones))
+            'total_likes' : contar_likes(interacciones, i[0]),
+            'total_comments' : contar_comentarios(interacciones, i[0]),
+            'numero_interacciones' : total_interacciones(contar_likes(interacciones, i[0]), contar_comentarios(interacciones, i[0])),
+            'es_top_usuario' : es_top_usuario(top_usuarios(interacciones), i[0] )
         }
         dic_usuarios.append(user)
-    print(dic_usuarios)
+    return dic_usuarios
  
-#def escribir_reporte()
-    
+reporte = escribir_reporte(usuarios)
 
-#print(contar_likes(interacciones))
-#print(contar_comentarios(interacciones))
-#print(total_interacciones(contar_likes(interacciones), contar_comentarios(interacciones)))
-#calcular_edad(obtener_edad(usuarios))
-#mas_viciosos(interacciones, usuarios)
+with open('reporte_interacciones.json', 'w', newline='') as f:
+    json.dump(reporte, f)
 
-to_diccionario(usuarios)
+with open('reporte_interacciones.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['id_usuario', 'nombre_usuario', 'edad', 'total_likes', 'total_comments', 'numero_interacciones', 'es_top_usuario'])
+    for i in reporte:
+        writer.writerow([i['id'], i['nombre'], i['edad'], i['total_likes'], i['total_comments'], i['numero_interacciones'], i['es_top_usuario']])
